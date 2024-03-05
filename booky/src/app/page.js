1886830037c6ca1e6722f9f700dd23bc67faef44
Mikadifo <mikadifopadillaheredia@gmail.com>
2024-03-05 14:36:01 -0500

    add: footer

--- booky/src/app/page.js
+++ booky/src/app/page.js
@@ -1,8 +1,10 @@
 export default function Home() {
   return (
     <main>
-      <footer>
-        <p>2024 Booky.example</p>
+      <footer className="bg-slate-700 text-gray-100 text-center py-3">
+        <p>
+          2024 <a href="#">Booky.example</a>
+        </p>
       </footer>
     </main>
   );
