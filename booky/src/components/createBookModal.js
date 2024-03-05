7526933604eacb76694218549ed44c64fb2d02a9
Mikadifo <mikadifopadillaheredia@gmail.com>
2024-03-05 18:36:08 -0500

    add: create book modal inputs and handlers

--- booky/src/components/createBookModal.js
+++ booky/src/components/createBookModal.js
@@ -1,6 +1,16 @@
+import { useState } from "react";
+
 const CURRENT_YEAR = new Date().getFullYear();
 
 const CreateBookModal = ({ setIsModalActive }) => {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [genre, setGenre] = useState("");
+  const [isbn, setIsbn] = useState("");
+  const [year, setYear] = useState(1);
+  const [copies, setCopies] = useState(1);
+  const [isAvailableOnline, setIsAvailableOnline] = useState(false);
+
   return (
     <dialog className="fixed left-0 top-0 w-full h-full bg-black bg-opacity-50 z-50 overflow-auto backdrop-blur flex justify-center items-center">
       <div className="bg-slate-200 m-auto py-8 px-16 rounded-lg">
@@ -21,7 +31,9 @@ const CreateBookModal = ({ setIsModalActive }) => {
                 id="title"
                 className="bg-gray-50 border border-gray-300 text-gray-200 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="The Great Gatsby"
+                value={title}
                 required
+                onChange={({ target }) => setTitle(target.value)}
               />
             </div>
             <div className="mb-5">
@@ -36,7 +48,9 @@ const CreateBookModal = ({ setIsModalActive }) => {
                 id="author"
                 className="bg-gray-50 border border-gray-300 text-gray-200 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Scott Fitzgeral"
+                value={author}
                 required
+                onChange={({ target }) => setAuthor(target.value)}
               />
             </div>
             <div className="mb-5">
@@ -51,7 +65,9 @@ const CreateBookModal = ({ setIsModalActive }) => {
                 id="genre"
                 className="bg-gray-50 border border-gray-300 text-gray-200 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Classic Literature"
+                value={genre}
                 required
+                onChange={({ target }) => setGenre(target.value)}
               />
             </div>
             <div className="mb-5">
@@ -66,7 +82,9 @@ const CreateBookModal = ({ setIsModalActive }) => {
                 id="isbn"
                 className="bg-gray-50 border border-gray-300 text-gray-200 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="13-digit code"
+                value={isbn}
                 required
+                onChange={({ target }) => setIsbn(target.value)}
               />
             </div>
             <div className="mb-5">
@@ -74,70 +92,25 @@ const CreateBookModal = ({ setIsModalActive }) => {
                 htmlFor="year"
                 className="block mb-2 text-md font-bold text-gray-800"
               >
-                Year:
+                Year
               </label>
-              <div className="relative flex items-center max-w-[8rem]">
-                <button
-                  type="button"
-                  id="decrement-button"
-                  data-input-counter-decrement="quantity-input"
-                  className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-s-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none"
-                >
-                  <svg
-                    className="w-3 h-3 text-gray-900 dark:text-white"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 18 2"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M1 1h16"
-                    />
-                  </svg>
-                </button>
-                <input
-                  type="text"
-                  id="year"
-                  data-input-counter
-                  data-input-counter-min="0"
-                  data-input-counter-max={`${CURRENT_YEAR}`}
-                  aria-describedby="helper-text-explanation"
-                  className="bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="1"
-                  required
-                />
-                <button
-                  type="button"
-                  id="increment-button"
-                  data-input-counter-increment="quantity-input"
-                  className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none"
-                >
-                  <svg
-                    className="w-3 h-3 text-gray-900 dark:text-white"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 18 18"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M9 1v16M1 9h16"
-                    />
-                  </svg>
-                </button>
-              </div>
+              <input
+                className="bg-gray-50 border border-gray-300 text-gray-200 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 text-center"
+                type="number"
+                id="year"
+                aria-describedby="helper-text-explanation"
+                placeholder="1"
+                required
+                min={1}
+                max={CURRENT_YEAR}
+                value={year}
+                onChange={({ target }) => setYear(target.value)}
+              />
               <p
                 id="helper-text-explanation"
                 className="mt-2 text-sm text-gray-500 dark:text-gray-400"
               >
-                Please select a number from 0 to {CURRENT_YEAR}
+                Please select a number from 1 to {CURRENT_YEAR}
               </p>
             </div>
             <div className="mb-5">
@@ -145,81 +118,39 @@ const CreateBookModal = ({ setIsModalActive }) => {
                 htmlFor="copies"
                 className="block mb-2 text-md font-bold text-gray-800"
               >
-                Copies:
+                Copies
               </label>
-              <div className="relative flex items-center max-w-[8rem]">
-                <button
-                  type="button"
-                  id="decrement-button"
-                  data-input-counter-decrement="quantity-input"
-                  className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-s-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none"
-                >
-                  <svg
-                    className="w-3 h-3 text-gray-900 dark:text-white"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 18 2"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M1 1h16"
-                    />
-                  </svg>
-                </button>
-                <input
-                  type="text"
-                  id="copies"
-                  data-input-counter
-                  data-input-counter-min="0"
-                  data-input-counter-max="35"
-                  aria-describedby="helper-text-explanation"
-                  className="bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="1"
-                  required
-                />
-                <button
-                  type="button"
-                  id="increment-button"
-                  data-input-counter-increment="quantity-input"
-                  className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none"
-                >
-                  <svg
-                    className="w-3 h-3 text-gray-900 dark:text-white"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 18 18"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M9 1v16M1 9h16"
-                    />
-                  </svg>
-                </button>
-              </div>
+              <input
+                type="number"
+                id="copies"
+                aria-describedby="helper-text-explanation"
+                className="bg-gray-50 border border-gray-300 text-gray-200 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 text-center"
+                min={1}
+                max={35}
+                placeholder="1"
+                required
+                value={copies}
+                onChange={({ target }) => setCopies(target.value)}
+              />
               <p
                 id="helper-text-explanation"
                 className="mt-2 text-sm text-gray-500 dark:text-gray-400"
               >
-                Please select a number from 0 to 35
+                Please select a number from 1 to 35
               </p>
             </div>
             <div className="mb-5">
               <label className="inline-flex items-center mb-5 cursor-pointer">
                 <input
                   type="checkbox"
-                  value=""
+                  checked={isAvailableOnline}
                   className="sr-only peer"
                   required
+                  onChange={({ target }) =>
+                    setIsAvailableOnline(target.checked)
+                  }
                 />
-                <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-black-300 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:w-5 after:h-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600" />
+                <div className="relative w-11 h-6 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-black-300 rounded-full peer bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:w-5 after:h-5 after:transition-all peer-checked:bg-blue-600" />
                 <span className="ms-3 text-md font-bold text-gray-800">
                   Available Online
                 </span>
