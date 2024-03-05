64fc4b868c58382f99a61ea5436b0aea3167ce48
Mikadifo <mikadifopadillaheredia@gmail.com>
2024-03-05 14:31:51 -0500

    add: header title

--- booky/src/app/page.js
+++ booky/src/app/page.js
@@ -1,7 +1,6 @@
 export default function Home() {
   return (
     <main>
-      <h1>HEY</h1>
       <footer>
         <p>2024 Booky.example</p>
       </footer>
---
+++ booky/src/components/header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <nav>
+      <h1 className="text-2xl text-black text-center m-4">BOOKY</h1>
+    </nav>
+  );
+};
+
+export default Header;
