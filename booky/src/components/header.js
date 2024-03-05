d899e8a03d1364725c876e2f1af2f0900fdf07ac
Mikadifo <mikadifopadillaheredia@gmail.com>
2024-03-05 14:36:38 -0500

    mod: header title

--- booky/src/components/header.js
+++ booky/src/components/header.js
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <nav>
-      <h1 className="text-2xl text-black text-center m-4">BOOKY</h1>
+      <h1 className="text-4xl text-black text-center m-4 font-bold">BOOKY</h1>
     </nav>
   );
 };
