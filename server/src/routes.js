80a55b981c17ca7cdcbf7c4afe87fba3eaffd43c
Mikadifo <mikadifopadillaheredia@gmail.com>
2024-03-05 13:37:50 -0500

    mod: added mongoose cast error handling

--- server/src/routes.js
+++ server/src/routes.js
@@ -3,14 +3,6 @@ const Book = require("./models/book.js");
 
 const recordRoutes = express.Router();
 
-recordRoutes.route("*").all((req, res) => {
-  throw {
-    statusCode: 404,
-    isOperational: true,
-    message: "This router does not exist in the server.",
-  };
-});
-
 recordRoutes.route("/").get((_, res) => {
   res.send("Welcome to Booky!");
 });
@@ -25,24 +17,33 @@ recordRoutes.route("/book/list").get(async (_, res) => {
 });
 
 recordRoutes.route("/book/update/:id").put(async (req, res) => {
-  const book = await Book.findByIdAndUpdate(req.params.id, req.body);
-  console.log(book);
-  res.send("ok");
-  //if (!book) {
-  //res.status(200).json({
-  //message: "",
-  //error: `Book by id ${req.params.id} not found. Please check the correct id is being send or try again later.`,
-  //});
-  //} else {
-  //res
-  //.status(200)
-  //.json({ message: "Book Updated!", error: "", data: { book } });
-  //}
+  try {
+    const book = await Book.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res
+      .status(200)
+      .json({ message: "Book Updated!", error: "", data: { book } });
+  } catch (error) {
+    res.status(200).json({
+      message: "",
+      error:
+        "Something went wrong updating the book. Please check the correct id is being sent or try again later.",
+    });
+  }
 });
 
 recordRoutes.route("/book/delete/:id").delete(async (req, res) => {
   await Book.deleteOne({ _id: req.params.id });
   res.status(200).send("Book Deleted");
+});
+
+recordRoutes.route("*").all((req, res) => {
+  throw {
+    statusCode: 404,
+    isOperational: true,
+    message: "This route does not exist in the server.",
+  };
 });
 
 module.exports = recordRoutes;
