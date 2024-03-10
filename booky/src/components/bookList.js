e8bdf229b77371b395dff0f5a202fd9ff58c7c3d
Mikadifo <mikadifopadillaheredia@gmail.com>
2024-03-10 17:09:30 -0400

    del: deleting svg as a component

--- booky/src/components/bookList.js
+++ booky/src/components/bookList.js
@@ -5,7 +5,6 @@ import BookModal from "./bookModal";
 import { BASE_URL } from "@/constants";
 import { bookFilter } from "@/utils/filters";
 import TableHead from "./tableHead";
-import SearchIcon from "@/resources/searchIcon";
 import Button from "./button";
 
 const BookList = () => {
@@ -54,7 +53,19 @@ const BookList = () => {
         />
         <div className="relative">
           <div className="absolute inset-y-0 left-0 rtl:inset-r-0 rtl:right-0 flex items-center ps-3 pointer-events-none">
-            <SearchIcon />
+            <svg
+              className="w-5 h-5 text-gray-500 dark:text-gray-400"
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
           </div>
           <input
             type="text"
